3474540ea1949968610dc86f517953672c0d34b5
Tolgahan Albayrak <tolgahan.albayrak@zebraelectronics.com>
2024-03-09 22:41:56 +0300

    assets: address-api

--- assets/js/address-api.js
+++ assets/js/address-api.js
@@ -1,9 +1,35 @@
-console.log(import.meta.url);
+const CACHE = {};
+
+import {JSONPath} from "https://cdn.statically.io/gh/JSONPath-Plus/JSONPath/main/dist/index-browser-esm.min.js";
+
+async function loadJson(url) {
+    const key = `json:${url}`;
+
+    if (key in CACHE) {
+        return await CACHE[key];
+    }
+
+    return CACHE[key] = fetch(url)
+        .then(r => r.json()).catch(() => null);
+}
 
 export default class AddressApi {
     constructor(baseUrl) {
-        baseUrl ??= import.meta?.url ?? "https://iadb.fast-page.org/";
-        this.baseUrl = baseUrl;
-        console.log(baseUrl);
+        const url = baseUrl ?? new URL(import.meta.url).origin;
+        this.baseUrl = url.replace(/\/+$/, "");
+        console.log(this.baseUrl);
+    }
+
+    list(path) {
+        return loadJson(`${this.baseUrl}/list/${path}`);
+    }
+
+    async loadCountries() {
+        if ("countries" in CACHE) {
+            return CACHE["countries"];
+        }
+
+        const schema = await this.list("countries.schema.json");
+        console.log(schema);
     }
 }
